868fd32e2edb9d2d9b7c2ea3fbf59ca12f07f2a6
zy <admin@editso.com>
2020-08-10 01:22:33 +0800

    各种类型节点

--- ast/tree.c
+++ ast/tree.c
@@ -1,4 +1,39 @@
 //
 // Created by zy on 8/9/20.
 //
+#include "tree.h"
 
+VarTerm* new_var_term(Token* name, void* expr){
+    VarTerm* var = malloc(sizeof(VarTerm));
+    var->name = name;
+    var->value = expr;
+    return var;
+}
+
+ConstraintTerm* new_constraintTerm(Token* name){
+    ConstraintTerm* constraint = malloc(sizeof(ConstraintTerm));
+    constraint->name = name;
+    return constraint;
+}
+
+BinaryExpr* new_binary_expr(void* left,Token* op, void* right){
+    BinaryExpr* binary = malloc(sizeof(BinaryExpr));
+    binary->left = left;
+    binary->op = op;
+    binary->right = right;
+    return binary;
+}
+
+BlockStmt* new_block_stmt(struct list* stmts){
+    BlockStmt* block = malloc(sizeof(BlockStmt));
+    block->stmts = stmts;
+    return block;
+}
+
+WhileStmt* new_while_stmt(Token* name, void* expr, BlockStmt* block){
+    WhileStmt* t_while = malloc(sizeof(WhileStmt));
+    t_while ->name = name;
+    t_while->expr =expr;
+    t_while->block=block;
+    return t_while;
+}
--- ast/tree.h
+++ ast/tree.h
@@ -4,9 +4,74 @@
 
 #ifndef TIMELY_LANG_TREE_H
 #define TIMELY_LANG_TREE_H
+#include "../token.h"
+#include "../list.h"
+
 
 typedef struct{
 
+}Environment;
+
+typedef struct{
+    void* (*eval)(Environment* env, void* node);
+}Eval;
+
+#define GET_EVAL(eval) ((Eval*)eval)
+#define GET_FUN(fun) ((void *(*)(Environment *, void *))fun)
+
+
+typedef struct{
+    Eval *eval;
+    struct list* stmts;
 }Tree;
+
+typedef struct {
+    Eval *eval;
+    void* left;
+    Token *op;
+    void* right;
+}BinaryExpr;
+
+
+typedef struct{
+    Eval *eval;
+    Token *name;
+    void* value;
+}VarTerm;
+
+
+typedef struct{
+    Eval *eval;
+    Token *name;
+}ConstraintTerm;
+
+typedef struct{
+    Eval *eval;
+    struct list* stmts;
+}BlockStmt;
+
+typedef struct {
+
+}EmptyStmt;
+
+typedef struct{
+    Eval *eval;
+    Token *name;
+    void* expr;
+    BlockStmt* block;
+}WhileStmt;
+
+
+VarTerm* new_var_term(Token* name, void* expr);
+
+ConstraintTerm* new_constraintTerm(Token* name);
+
+BinaryExpr* new_binary_expr(void* left,Token* op, void* right);
+
+BlockStmt* new_block_stmt(struct list* stmts);
+
+WhileStmt* new_while_stmt(Token* name, void* expr, BlockStmt* block);
+
+
 
 #endif //TIMELY_LANG_TREE_H
