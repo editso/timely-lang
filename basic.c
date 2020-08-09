7a1f9c9af6b7cc7f465bf0ef10befc7e910a14de
zy <admin@editso.com>
2020-08-10 01:24:08 +0800

    新增timely所需要的基本函数

--- basic.c
+++ basic.c
@@ -2,22 +2,65 @@
 // Created by zy on 8/8/20.
 //
 #include "basic.h"
+#include "log.h"
 #include "string.h"
+#include "errno.h"
 
 
+KindMeta get_kind_meta(Kind kind){
+    if (kind >= 0 && kind <= 127){
+        KindMeta* meta = malloc(sizeof(KindMeta));
+        char* s = malloc(sizeof(s));
+        *s = (char)kind;
+        meta->kind = kind;
+        meta->name = s;
+        return *meta;
+    }else{
+        for (int i = 0; i < kind_count; ++i) {
+            if (kind_table[i].kind == kind){
+                return kind_table[i];
+            }
+        }
+    }
+    return EMPTY_KIND;
+}
 
-void print(char *mat, ...){
+
+void lexer_error(char *m, char *s, unsigned int row, unsigned int col){
+    size_t len = strlen(s);
+    char buff[col + 2];
+    memset(buff, '\0', col+2);
+    memset(buff,'\n',1);
+    memset(buff+1,' ', col);
+    memset(buff+col, '^',  1);
+    char n_buff[col];
+    if (col > len){
+        memset(n_buff, ' ', col - len + 1);
+    }
+    fprintf(stderr,"%s:(%d, %d)\n", m,row,col);
+    fprintf(stderr,"%s%s%s\n",n_buff,s,buff);
+    abort();
+}
+
+void print(char *s, ...){
     va_list args;
     va_list args1;
-    va_start(args, mat);
+    va_start(args, s);
     va_copy(args1, args);
-    char buf[1 + vsnprintf(NULL, 0, mat, args)];
+    char buf[1 + vsnprintf(NULL, 0, s, args)];
     va_end(args);
-    vsnprintf(buf, sizeof(buf), mat, args1);
+    vsnprintf(buf, sizeof(buf), s, args1);
     va_end(args1);
     printf("%s\n", buf);
 }
 
+
+void out_token(Token* token){
+    print("<Kind=%s, (%d,%d)>",
+          token->text,
+          token->row_pos,
+          token->col_pos);
+}
 
 long to_number(struct list *list){
     char *buf = malloc(sizeof(char) * list->size);
--- basic.h
+++ basic.h
@@ -7,13 +7,23 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "list.h"
+#include "token.h"
+#include "log.h"
+#include "string.h"
 
+#define GET_CHAR(chr) memset(malloc(sizeof(char)), chr, 1)
 #define t_bool int
 #define t_true  1
 #define t_false 0
 
 
 void print(char *, ...);
+
+
+void out_token(Token* token);
+
+
+
 
 long to_number(struct list *list);
 
